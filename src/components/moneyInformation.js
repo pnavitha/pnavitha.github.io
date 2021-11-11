fcb42cb0bfd92ccc48dbb0efe5b01512c9cbe7a9
Navita <navita.pareek4@gmail.com>
2021-11-11 21:29:54 +0530

    Ideal Journey section changed

--- src/components/moneyInformation.js
+++ src/components/moneyInformation.js
@@ -23,8 +23,8 @@ const useStyles = makeStyles({
     },
     Card: {
         padding: '8%',
-        width: '120px',
-        height: '130px',
+        // width: '150px',
+        // height: '180px',
     }
 });
 
@@ -34,45 +34,88 @@ const MoneyInformation = () => {
 
     return <Paper square className={classes.ContentWrapper}>
         <Typography variant="h1">What an Ideal Journey to Become a Product Manager looks like with MyNextInning?</Typography>
-        <Grid container direction="row" justify="space-evenly" alignItems="center" spacing={4}>
-            <Grid item xs={12} sm={4}>
-                <br />
-                <Grid container direction='column'>
-                    <br />
-                    <Typography variant="body1">Step 1 - Sign Up</Typography>
-                    <Typography variant="body1">Step 2 - We get you the Best Mentor by analyzing your Strengths and Weaknesses.</Typography>
-                    <Typography variant="body1">Step 3 - The Mentor will Personally Create a Holistic Roadmap that will help you become a Product Manager.</Typography>
-                    <br />
-                </Grid>
+        <br />
+        <Grid container direction="row" justify='center' spacing={2}>
+        <Grid item xs={12} sm={2}>
+        <Grid container direction="column" spacing={0}>
+                <Paper square elevation={2} className={classes.Card}>
+                    <Grid container direction='column' justify="space-between" alignItems='center'>
+                        <Typography variant="subtitle1" color='primary'>Interact with Mentor</Typography>
+                        <Typography variant="body1">Book mentor's time and use free trial discussion</Typography>
+                        <br />
+                        <img src={require("../images/find_mentor.svg")} alt="Product Management Career Mentorship Program Become a Product Manager Career Mentor for better career high salary " width="100%" />
+
+                    </Grid>
+                </Paper>
             </Grid>
-            <Grid item xs={12} sm={4}>
-                <br />
-                <Grid container direction='column'>
+            
+       </Grid>
+       <Grid item xs={12} sm={2}>
+        <Grid container direction="column" spacing={0}>
+            <br/>
+            <Paper square elevation={2} className={classes.Card}>
+                <Grid container direction='column' justify="space-between" alignItems='center'>
+                    <Typography variant="subtitle1" color='primary'>Get Career & Profile Feedback</Typography>
+                    <Typography variant="body1">Analyze your Strengths and Weaknesses</Typography>
                     <br />
                     <img src={require("../images/aboutus.svg")} alt="Product Management Career Mentorship Program Become a Product Manager Career Mentor for better career high salary " width="100%" />
-                    <br />
+
                 </Grid>
-            </Grid>
+            </Paper>
        </Grid>
-        <br/>
-        <Grid container direction="row" justify="space-evenly" alignItems="center" spacing={4}>
-            <Grid item xs={12} sm={4}>
-                <br />
-                <Grid container direction='column'>
+       </Grid>
+       <Grid item xs={12} sm={2}>
+       <Grid container direction="column" spacing={0}>
+       <br/>
+       <br/>
+       <br />
+            <Paper square elevation={2} className={classes.Card}>
+                <Grid container direction='column' justify="space-between" alignItems='center'>
+                    <Typography variant="subtitle1" color='primary'>Get Personalised Plan</Typography>
+                    <Typography variant="body1">A Roadmap to become a Product Manager</Typography>
                     <br />
-                    <Typography variant="body1">Step 4 - You Follow the Roadmap and become Interview Ready</Typography>
-                    <Typography variant="body1">Step 5 - Give the Interview and become a Product Manager!</Typography>
-                    <br />
+                    <img src={require("../images/msme_loans.svg")} alt="Product Management Career Mentorship Program Become a Product Manager Career Mentor for better career high salary " width="100%" />
+
                 </Grid>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-                <br />
-                <Grid container direction='column'>
+            </Paper>
+        </Grid>
+       </Grid>
+       <Grid item xs={12} sm={2}>
+       <Grid container direction="column" spacing={0}>
+       <br/>
+       <br/>
+       <br/>
+       <br />
+
+            <Paper square elevation={2} className={classes.Card}>
+                <Grid container direction='column' justify="space-between" alignItems='center'>
+                    <Typography variant="subtitle1" color='primary'>Execute Plan with Discussions</Typography>
+                    <Typography variant="body1">You Follow the Roadmap and become Interview Ready</Typography>
+                    <br />
+                    <img src={require("../images/work_together.svg")} alt="Product Management Career Mentorship Program Become a Product Manager Career Mentor for better career high salary " width="100%" />
+
+                </Grid>
+            </Paper>
+        </Grid>
+       </Grid>
+       <Grid item xs={12} sm={2}>
+       <Grid container direction="column" spacing={0}>
+       <br/>
+       <br/>
+       <br/>
+       <br />
+       <br/>
+            <Paper square elevation={2} className={classes.Card}>
+                <Grid container direction='column' justify="space-between" alignItems='center'>
+                    <Typography variant="subtitle1" color='primary'>Final Result, Smoother & Quicker</Typography>
+                    <Typography variant="body1">Give the Interview and become a Product Manager!</Typography>
                     <br />
                     <img src={require("../images/rocket.svg")} alt="Product Management Career Mentorship Program Become a Product Manager Career Mentor for better career high salary " width="100%" />
-                    <br />
+
                 </Grid>
-            </Grid>
+            </Paper>
+        </Grid>
+       </Grid>
        </Grid>
     </Paper>
 }
