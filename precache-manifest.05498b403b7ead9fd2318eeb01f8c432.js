d919b57da414a2ca0b3ca8df818c324cf07b9475
Navita <navita.pareek4@gmail.com>
2022-01-17 21:48:33 +0530

    Updates

---
+++ precache-manifest.05498b403b7ead9fd2318eeb01f8c432.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb1c21101aed6ddb105cd7c95efe040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "938d8a96b8501a274ca4",
+    "url": "/static/js/2.efdecf4e.chunk.js"
+  },
+  {
+    "revision": "2e2a90e089db886c69443e6e3bf709b6",
+    "url": "/static/js/2.efdecf4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b25ba6052f661396b8d",
+    "url": "/static/js/main.2eafeef3.chunk.js"
+  },
+  {
+    "revision": "b9f945413c60cfb0769c",
+    "url": "/static/js/runtime-main.ea755a08.js"
+  },
+  {
+    "revision": "999182efbcf97032c71086acbd21d55d",
+    "url": "/static/media/1.999182ef.png"
+  },
+  {
+    "revision": "ecc6ccd703e63f5198acd35c7e827453",
+    "url": "/static/media/1.ecc6ccd7.png"
+  },
+  {
+    "revision": "8aafbd4e15153ba5888c2272056ba32a",
+    "url": "/static/media/2.8aafbd4e.png"
+  },
+  {
+    "revision": "9fbd0716d3401026bc2cf3e1b2ee4612",
+    "url": "/static/media/2.9fbd0716.png"
+  },
+  {
+    "revision": "10fa4f79b64eb5fae8016269f0110310",
+    "url": "/static/media/3.10fa4f79.png"
+  },
+  {
+    "revision": "1202988f8477c4d1b8a1d32ac01e1a73",
+    "url": "/static/media/3.1202988f.png"
+  },
+  {
+    "revision": "4d1a6520ed02c33f2802049e981f6778",
+    "url": "/static/media/4.4d1a6520.png"
+  },
+  {
+    "revision": "6c74560fc59a46f5a67037383191270e",
+    "url": "/static/media/4.6c74560f.png"
+  },
+  {
+    "revision": "578fe1567c40854e82ccad8a7d7f446f",
+    "url": "/static/media/AspiringProductManagerBrochure.578fe156.pdf"
+  },
+  {
+    "revision": "6c933617ea5fcb894ebb85f09e344b16",
+    "url": "/static/media/AssociateProductManagerBrochure.6c933617.pdf"
+  },
+  {
+    "revision": "a57d42b5a5045d94fd893290f29d0609",
+    "url": "/static/media/aboutus.a57d42b5.svg"
+  },
+  {
+    "revision": "8fffbb9104f2fc574a8fdf00ecbaba86",
+    "url": "/static/media/arpita.8fffbb91.png"
+  },
+  {
+    "revision": "e9a5e302656ed1caba8bbfdded5be62b",
+    "url": "/static/media/cowork.e9a5e302.svg"
+  },
+  {
+    "revision": "f1d0587dc871954412cef5be114d7ec1",
+    "url": "/static/media/find_mentor.f1d0587d.svg"
+  },
+  {
+    "revision": "ed9673a0796d6c3a334c20fb20b6653d",
+    "url": "/static/media/kushagra.ed9673a0.png"
+  },
+  {
+    "revision": "6d2d338d6346d63b958889e65f02cea8",
+    "url": "/static/media/msme_loans.6d2d338d.svg"
+  },
+  {
+    "revision": "ab70069288ecd0fdc780b76821a62a52",
+    "url": "/static/media/pooshkar.ab700692.png"
+  },
+  {
+    "revision": "6ad281df02d0881d238ff9140de53d55",
+    "url": "/static/media/rocket.6ad281df.svg"
+  },
+  {
+    "revision": "fd4648b6b638b9e92a6046cb23c33dc2",
+    "url": "/static/media/testimony_saloni.fd4648b6.png"
+  },
+  {
+    "revision": "633c380bb4255a1922833b414d794ecc",
+    "url": "/static/media/win.633c380b.svg"
+  },
+  {
+    "revision": "162589b039348972a185f076fdc6a9e8",
+    "url": "/static/media/work_together.162589b0.svg"
+  }
+]);
---
+++ worker.js
@@ -0,0 +1,40 @@
+var CACHE_NAME = 'peso-pwa-task-manager';
+var urlsToCache = [
+  '/'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request);
+      })
+    );
+  });
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['peso-pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
