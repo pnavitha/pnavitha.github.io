1df3f5363163a40291f9e43c329daca700db82e1
Navita <navita.pareek4@gmail.com>
2022-01-17 21:19:59 +0530

    commented backend calls

--- src/context/app-context/app-middleware.js
+++ src/context/app-context/app-middleware.js
@@ -1,38 +1,38 @@
 export const AppMiddleware = (dispatch, state) => (action) => {
     switch (action.type) {
         case 'BUTTON_CLICKED':
-            (async () => {
-                const payload = {
-                    feedback: {
-                        button: action.payload
-                    },
-                    action: "SUBMIT_FEEDBACK"
-                };
+            // (async () => {
+            //     const payload = {
+            //         feedback: {
+            //             button: action.payload
+            //         },
+            //         action: "SUBMIT_FEEDBACK"
+            //     };
 
-                await fetch('https://q44f17qqyi.execute-api.ap-south-1.amazonaws.com/prod/users', {
-                    method: 'POST',
-                    body: JSON.stringify(payload),
-                });
-            })();
+            //     await fetch('https://q44f17qqyi.execute-api.ap-south-1.amazonaws.com/prod/users', {
+            //         method: 'POST',
+            //         body: JSON.stringify(payload),
+            //     });
+            // })();
             break;
         case 'SUBMIT_LINKEDIN_URL':
-            console.log("linkedInUrl: ", state.linkedInUrl);
-            if(state.linkedInUrl && state.linkedInUrl.length > 0){
-            (async () => {
-                const payload = {
-                    feedback: {
-                        linkedInUrl: state.linkedInUrl
-                    },
-                    action: "SUBMIT_FEEDBACK"
-                };
+            // console.log("linkedInUrl: ", state.linkedInUrl);
+            // if(state.linkedInUrl && state.linkedInUrl.length > 0){
+            // (async () => {
+            //     const payload = {
+            //         feedback: {
+            //             linkedInUrl: state.linkedInUrl
+            //         },
+            //         action: "SUBMIT_FEEDBACK"
+            //     };
 
-                await fetch('https://q44f17qqyi.execute-api.ap-south-1.amazonaws.com/prod/users', {
-                    method: 'POST',
-                    body: JSON.stringify(payload),
-                });
-                dispatch({ type: "LINKEDIN_SUBMITTED" })
-            })();
-            }
+            //     await fetch('https://q44f17qqyi.execute-api.ap-south-1.amazonaws.com/prod/users', {
+            //         method: 'POST',
+            //         body: JSON.stringify(payload),
+            //     });
+            //     dispatch({ type: "LINKEDIN_SUBMITTED" })
+            // })();
+            // }
             break;    
         default: {
             dispatch(action);
